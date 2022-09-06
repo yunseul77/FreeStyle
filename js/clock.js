0571d917f3f0dd1f96cf71c298e00e6bfd95c098
yunseul <engus7303@gmail.com>
2022-09-06 20:04:49 +0900

    web page

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,7 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}`;
-  clockSeconds.innerText = `:${seconds}`;
+  clockSeconds.innerText = `${seconds}`;
 }
 
 getClock();
